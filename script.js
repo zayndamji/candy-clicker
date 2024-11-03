99702c16c27ac11f7a310fc336e2317d8ad9cc34
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2024-11-03 15:50:21 -0800

    auto generates upgrades from object

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const upgrades = localStorage.getItem('upgrades') ? JSON.parse(localStorage.getItem('upgrades')) : [
   {
     name: "Child",
+    description: "A child will run from house to house, gathering 1 candy per second.",
     originalPrice: 10,
     price: 10,
     quantity: 0,
@@ -8,6 +9,7 @@ const upgrades = localStorage.getItem('upgrades') ? JSON.parse(localStorage.getI
   },
   {
     name: "Parent",
+    description: "A parent will buy you candy when you ask, gathering 5 candy per second.",
     originalPrice: 250,
     price: 250,
     quantity: 0,
@@ -17,12 +19,75 @@ const upgrades = localStorage.getItem('upgrades') ? JSON.parse(localStorage.getI
 
 let candyScore = localStorage.getItem('candyScore') ? parseInt(localStorage.getItem('candyScore')) : 0;
 
+generateUpgrades();
 refreshDisplays();
 
 document.getElementById('candy').addEventListener('click', () => {
   candyScore++;
   refreshDisplays();
 });
+
+function generateUpgrades() {
+  for (let i = 0; i < upgrades.length; i++) {
+    const upgrade = document.createElement('div');
+    upgrade.classList.add('upgrade');
+
+    const contents = document.createElement('div');
+    contents.classList.add('contents');
+
+    const title = document.createElement('div');
+    title.classList.add('title');
+    title.textContent = upgrades[i].name + ' (' + upgrades[i].cps + ' CPS)';
+
+    const description = document.createElement('div');
+    description.classList.add('description');
+    description.textContent = upgrades[i].description;
+
+    const price = document.createElement('div');
+    price.classList.add('price');
+    const priceChild = document.createElement('span');
+    priceChild.id = 'price' + i;
+    price.append('Price: ', priceChild, ' Candies');
+
+    const quantity = document.createElement('div');
+    quantity.classList.add('quantity');
+    const quantityChild = document.createElement('span');
+    quantityChild.id = 'quantity' + i;
+    quantity.append('Quantity Owned: ', quantityChild, ' Parents');
+
+    contents.append(title, document.createElement('div'), description, document.createElement('div'), price, quantity);
+
+    const buttons = document.createElement('div');
+    buttons.classList.add('buttons');
+
+    const buy1 = document.createElement('button');
+    buy1.onclick = () => { addUpgrade(i, 1); };
+    buy1.classList.add('button', 'button-invalid');
+    buy1.id = 'buy1button' + i;
+    buy1.disabled = true;
+    buy1.textContent = 'Buy 1';
+
+    const buy10 = document.createElement('button');
+    buy10.onclick = () => { addUpgrade(i, 10); };
+    buy10.classList.add('button', 'button-invalid');
+    buy10.id = 'buy10button' + i;
+    buy10.disabled = true;
+    buy10.textContent = 'Buy 10';
+
+    const buyMax = document.createElement('button');
+    buyMax.onclick = () => { addUpgrade(i); };
+    buyMax.classList.add('button', 'button-invalid');
+    buyMax.id = 'buymaxbutton' + i;
+    buyMax.disabled = true;
+    buyMax.textContent = 'Buy Max';
+
+    buttons.append(buy1, buy10, buyMax);
+
+    upgrade.append(contents, buttons);
+
+    document.getElementById('upgradesParent').append(upgrade);
+  }
+}
 
 function refreshDisplays() {
   document.getElementById('score').textContent = candyScore;
