8832744328f5722e9fe11930cd5f2c9730f0325e
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2024-11-02 23:19:38 -0700

    added code for Child upgrade

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-let candyScore = localStorage.getItem('candyScore') || 0;
+const upgrades = [
+  {
+    name: "Child",
+    price: 10,
+    quantity: 0,
+    sps: 1
+  }
+]
+
+let candyScore = parseInt(localStorage.getItem('candyScore')) || 0;
+let scorePerSecond = parseInt(localStorage.getItem('scorePerSecond')) || 0;
 const candy = document.getElementById('candy');
 const score = document.getElementById('score');
 setScore();
@@ -11,4 +21,20 @@ candy.addEventListener('click', () => {
 function setScore() {
   score.innerHTML = candyScore;
   localStorage.setItem('candyScore', candyScore);
+  localStorage.setItem('scorePerSecond', scorePerSecond);
 }
+
+function addUpgrade(index, quantityRequested) {
+  const { price, quantity, sps } = upgrades[index];
+
+  if (price * quantityRequested > candyScore) return;
+
+  scorePerSecond += quantityRequested * sps;
+  candyScore -= price * quantityRequested;
+  setScore();
+}
+
+setInterval(() => {
+  candyScore += scorePerSecond;
+  setScore();
+}, 1000);
